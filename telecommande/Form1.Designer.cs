00c155695b4cb5e53e812cddf20d6d8c78dfc7da
nsebti <nacersebti007@gmail.com>
2016-11-21 12:31:14 +0900

    Add project files.

---
+++ telecommande/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace telecommande
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Ajouter = new System.Windows.Forms.Button();
+            this.Diminuer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(183, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 25);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "15";
+            // 
+            // Ajouter
+            // 
+            this.Ajouter.Location = new System.Drawing.Point(73, 188);
+            this.Ajouter.Name = "Ajouter";
+            this.Ajouter.Size = new System.Drawing.Size(75, 36);
+            this.Ajouter.TabIndex = 1;
+            this.Ajouter.Text = "+";
+            this.Ajouter.UseVisualStyleBackColor = true;
+            this.Ajouter.Click += new System.EventHandler(this.Ajouter_Click);
+            // 
+            // Diminuer
+            // 
+            this.Diminuer.Location = new System.Drawing.Point(183, 188);
+            this.Diminuer.Name = "Diminuer";
+            this.Diminuer.Size = new System.Drawing.Size(75, 36);
+            this.Diminuer.TabIndex = 2;
+            this.Diminuer.Text = "-";
+            this.Diminuer.UseVisualStyleBackColor = true;
+            this.Diminuer.Click += new System.EventHandler(this.Diminuer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Temperature";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.Diminuer);
+            this.groupBox1.Controls.Add(this.Ajouter);
+            this.groupBox1.Location = new System.Drawing.Point(186, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(372, 396);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "GetMoona";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Ajouter;
+        private System.Windows.Forms.Button Diminuer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
